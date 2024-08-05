48a3972632c6b0c998ba5f0ef942f7570d852797
ermellino-ingegnoso <rabotancre@gmail.com>
2024-08-05 21:35:10 +0200

    Tentativo di packaging
    fallito miseramente

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,13 @@
 module unibo.javafxmvc {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.google.gson;
     requires java.sql;
     requires java.desktop;
     requires javafx.swing;
 
     opens unibo.javafxmvc to javafx.fxml;
-    opens unibo.javafxmvc.model to com.google.gson; //  gson accede agli attributi privati dei model
 
     opens unibo.javafxmvc.controller to javafx.fxml;
-    opens unibo.javafxmvc.DAO to com.google.gson;
 
     exports unibo.javafxmvc.exception;
     exports unibo.javafxmvc.model;
